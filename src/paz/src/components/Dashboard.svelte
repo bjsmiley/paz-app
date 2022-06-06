cd877b202062602ac7939217eeb5dbef5d7b6f35
Bryan Smiley <bjsmiley@ucdavis.edu>
2022-06-05 20:48:18 -0400

    add new, update, save buttons

--- src/paz/src/components/Dashboard.svelte
+++ src/paz/src/components/Dashboard.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
 
-import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
+import Accordion from '@smui-extra/accordion';
+import IconButton from '@smui/icon-button';
 import { get } from 'svelte/store'
-import { state } from '../store'
+import { state, transport } from '../store'
 import Reminder from './Reminder.svelte'
+import type { ClientState, CoreResponse, ReminderState } from '@paz/core';
+import {v4 as uuidv4} from 'uuid'
+import Button, { Label } from '@smui/button';
 
 let s = get(state)
 
 state.subscribe(val => {
     s = val
 });
+
+const onAddClick = () => {
+    // the add button doesnt really need to be disabled
+    // the newReminder can just be appended onto the array of reminders on state object
+    // then add delete button
+    let newReminder = { id: uuidv4(), name: "New Reminder", wait_sec: 5 * 60, is_active: false}
+    s?.reminders.push(newReminder)
+    s = s
+}
+
+const onCancelClick = async () => {
+    let res = (await get(transport).query({key: "ClientGetState"})) as CoreResponse;
+    state.set(res.data as ClientState)
+}
 
 </script>
 
@@ -20,18 +38,37 @@ state.subscribe(val => {
                 <Reminder state={reminder} />
             {/each}    
         </Accordion>
+        <!-- <IconButton style="margin-top: 5px;" class="material-icons" on:click={onAddClick} ripple={false} size="button">
+            add
+        </IconButton> -->
+        <div style="margin-top: 5px;">
+            <Button on:click={onAddClick}>
+                <Label>New</Label>
+            </Button>
+            <Button on:click={() => {}}>
+                <Label>Save</Label>
+            </Button>
+            <Button on:click={onCancelClick}>
+                <Label>Cancel</Label>
+            </Button>
+        </div>
+    
     </div>
 </div>
 
 
 <style>
 
+    .reminders-display {
+        margin: 30px 30px 30px 30px;
+    }
+
     .reminders-display :global(.reminders-child) {
         /* justify-content: center; */
         display: flex;
         justify-content: center;
         flex-direction: column;
-        margin: 30px 30px 30px 30px;
+        /* margin: 30px 30px 30px 30px; */
     }
 	
 </style>
--- src/paz/src/components/Reminder.svelte
+++ src/paz/src/components/Reminder.svelte
@@ -8,6 +8,12 @@ import Textfield from '@smui/textfield';
 import LayoutGrid, { Cell } from '@smui/layout-grid';
 import Icon from '@smui/textfield/icon';
 import HelperText from '@smui/textfield/helper-text';
+import IconButton from "@smui/icon-button";
+import { get } from 'svelte/store'
+import { state as clientState } from '../store'
+
+
+
   
 let edit = false;
 let checked = false;
@@ -15,21 +21,27 @@ let menu: MenuComponentDev;
 
 
 export let state: ReminderState;
+export let open= false;
 
 let hourSelect = Math.floor(state.wait_sec / 3600)
 let minuteValue = Math.floor((state.wait_sec - (hourSelect * 3600)) / 60)
 
 $: {
   state.wait_sec = ( hourSelect * 3600 ) + ( minuteValue * 60 )
-  console.log(state.wait_sec)
+}
+
+const onDelete = () => {
+  let s = get(clientState);
+  s.reminders = s.reminders.filter(r => r.id != state.id);
+  clientState.set(s)
 }
 
 </script>
 
-<Panel>
+<Panel bind:open>
   <Header>
     {state.name}
-    <span slot="description">{state.wait_sec}</span>
+    <span slot="description">{state.id}</span>
   </Header>
   <Content>
     <LayoutGrid>
@@ -51,6 +63,11 @@ $: {
       <Cell span={8}>
         <Textfield bind:value={hourSelect} label="HH" type="number" />
         <Textfield bind:value={minuteValue} label="mm" type="number" />
+      </Cell>
+      <Cell span={12}>
+        <IconButton style="margin-top: 5px;" class="material-icons" ripple={false} size="button" on:click={onDelete}>
+          delete
+        </IconButton>
       </Cell>
     </LayoutGrid>
 
